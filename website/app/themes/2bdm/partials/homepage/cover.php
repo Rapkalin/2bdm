f4b6f3199d8c354ebc1272463d58cb948458ef08
Rapkalin <raphael@myminicom.com>
2025-01-14 21:02:35 +0100

    Init project frontend structure

---
+++ website/app/themes/2bdm/partials/homepage/cover.php
@@ -0,0 +1,25 @@
+<?php
+
+if (have_rows('home_cover')) : while (have_rows('home_cover')) : the_row();
+    if(get_sub_field('image')['url']) :
+?>
+
+        <section id="home-cover" class="cover">
+            <div class="banner" style="background-image: url(<?= get_sub_field('image')['url']; ?>);">
+                <div class="content">
+                    <div class="section-title">
+                        <h2><?php the_sub_field('title'); ?></h2>
+                    </div>
+                    <p><?php the_sub_field('title'); ?></p>
+                    <?php if (get_sub_field('button')) : ?>
+                        <a href="<?= get_sub_field('button')['url']; ?>" class="button"><?= get_sub_field('button')['label']; ?></a>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </section>
+
+    <?php
+        endif;
+    endwhile;
+endif;
+

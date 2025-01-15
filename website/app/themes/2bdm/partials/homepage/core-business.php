0502df2c1aadcb8ecc3bd43e1f552c6a5c36ad0a
Rapkalin <raphael@myminicom.com>
2025-01-15 12:39:27 +0100

    core-business - Update block

--- website/app/themes/2bdm/partials/homepage/core-business.php
+++ website/app/themes/2bdm/partials/homepage/core-business.php
@@ -1,21 +1,24 @@
 <?php
 
-if (have_rows('home_core_business')) : while (have_rows('home_core_business')) : the_row();
-    if(get_sub_field('image')['url']) :
+if (have_rows('home_core_business')) :
+    while (have_rows('home_core_business')) : the_row();
+        if(get_sub_field('image')['url']) :
 ?>
-
-        <section id="home-core-business" class="home-block">
-            <div class="banner" style="background-image: url(<?= get_sub_field('image')['url']; ?>);">
+            <section id="home-core-business" class="home-block">
                 <div class="content">
+                    <div class="section-title">
+                        <h2><?php the_sub_field('title'); ?></h2>
+                    </div>
                     <p><?php the_sub_field('description'); ?></p>
-                    <?php if (get_sub_field('button')['url']) : ?>
-                        <a href="<?= get_sub_field('button')['url']; ?>" class="button"><?= get_sub_field('button')['label']; ?></a>
-                    <?php endif; ?>
+                    <img
+                        alt="<?= get_sub_field('image')['title'];?>"
+                        src="<?= get_sub_field('image')['url'];?>"
+                        width="<?= get_sub_field('image')['width'];?>"
+                        height="<?= get_sub_field('image')['height'];?>"
+                    >
                 </div>
-            </div>
-        </section>
-
-    <?php
+            </section>
+<?php
         endif;
     endwhile;
 endif;

1e0b91af4f27d98498d5e0afed9cd113aa49842c
Rapkalin <raphael@myminicom.com>
2025-01-20 20:32:58 +0100

    Add articles to homepage

--- website/app/themes/2bdm/index.php
+++ website/app/themes/2bdm/index.php
@@ -2,17 +2,12 @@
 get_header();
 $home = new WP_Query(['pagename' => 'homepage']);
 while ($home->have_posts()) : $home->the_post();
-    $fields = get_fields($home->post->ID);
-    foreach ($fields as $key => $field) {
+    foreach (get_fields($home->post->ID) as $key => $field) {
         if (file_exists(__DIR__ . "/partials/homepage/{$key}.php")) {
             get_template_part("partials/homepage/{$key}");
         }
     }
 endwhile; ?>
-
-<div>
-    <h2>A la une</h2>
-</div>
 
 <div>
     <h2>Nos agences</h2>
---
+++ website/app/themes/2bdm/partials/homepage/home_articles.php
@@ -0,0 +1,22 @@
+<?php
+if (have_rows('home_articles')) :
+    while (have_rows('home_articles')) : the_row();
+     $featured_articles = get_field('home_articles');
+        if ($featured_articles) : ?>
+            <section id="home-articles" class="home-block">
+                <?php foreach ($featured_articles as $article) : ?>
+                    <div class="home-article">
+                        <h2><?php the_field('title', $article->ID); ?></h2>
+                        <p><?php the_field('description', $article->ID); ?></p>
+                        <img
+                            alt="<?= get_field('image', $article->ID)['title'] ?>"
+                            src="<?= get_field('image', $article->ID)['url'] ?>"
+                            width="<?= get_field('image', $article->ID)['width'] ?>"
+                            height="<?= get_field('image', $article->ID)['height'] ?>"
+                        >
+                    </div>
+                <?php endforeach; ?>
+            </section>
+        <?php endif;
+    endwhile;
+endif;

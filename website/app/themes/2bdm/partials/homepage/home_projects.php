4ec08b34666bc162b17078ae7adf325be3f2b033
Rapkalin <raphael@myminicom.com>
2025-01-21 12:28:37 +0100

    Refacto projects and articles remove useless while

--- website/app/themes/2bdm/partials/homepage/home_projects.php
+++ website/app/themes/2bdm/partials/homepage/home_projects.php
@@ -1,23 +1,21 @@
 <?php
 
 if (have_rows('home_projects')) :
-    while (have_rows('home_projects')) : the_row();
-        $featured_projects = get_field('home_projects');
-        if ($featured_projects) : ?>
-            <section id="home-projects" class="home-block">
-                <?php foreach ($featured_projects as $project) : ?>
-                    <div class="home-project">
-                        <h2><?php the_field('title', $project->ID); ?></h2>
-                        <p><?php the_field('description', $project->ID); ?></p>
-                        <img
-                            alt="<?= get_field('image', $project->ID)['title'] ?>"
-                            src="<?= get_field('image', $project->ID)['url'] ?>"
-                            width="<?= get_field('image', $project->ID)['width'] ?>"
-                            height="<?= get_field('image', $project->ID)['height'] ?>"
-                        >
-                    </div>
-                <?php endforeach; ?>
-            </section>
-        <?php endif;
-    endwhile;
+    $featured_projects = get_field('home_projects');
+    if ($featured_projects) : ?>
+        <section id="home-projects" class="home-block">
+            <?php foreach ($featured_projects as $project) : ?>
+                <div class="home-project">
+                    <h2><?php the_field('title', $project->ID); ?></h2>
+                    <p><?php the_field('description', $project->ID); ?></p>
+                    <img
+                        alt="<?= get_field('image', $project->ID)['title'] ?>"
+                        src="<?= get_field('image', $project->ID)['url'] ?>"
+                        width="<?= get_field('image', $project->ID)['width'] ?>"
+                        height="<?= get_field('image', $project->ID)['height'] ?>"
+                    >
+                </div>
+            <?php endforeach; ?>
+        </section>
+    <?php endif;
 endif;

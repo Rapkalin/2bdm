c16590d00c12e880b113e4efe58a390520a14503
Rapkalin <raphael@myminicom.com>
2025-01-24 16:34:10 +0100

    Add meta info and replace logo svp to webp

--- website/app/themes/2bdm/header.php
+++ website/app/themes/2bdm/header.php
@@ -5,6 +5,8 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <meta name="description" content="<?= get_bloginfo('description') ?>">
+    <meta name="keywords" content="2BDM ARCHITECTURE, 2BDM, ARCHITECTURE, studio d'architectes, studio, studio d'artchitecture">
     <?php wp_head(); ?>
 </head>
 
@@ -12,7 +14,15 @@
     <div class="header-container">
         <div class="header-left">
             <div class="logo">
-                <a href="<?= home_url('/'); ?>"><img src="<?= asset('logo_2BDM.png') ?>" alt="2BDM ARCHITECTURE"></a>
+                <?php $imageData = getimagesize(get_template_directory() . '/assets/images/' . 'logo_2BDM.webp'); ?>
+                <a href="<?= home_url('/'); ?>">
+                    <img
+                        src="<?= asset('logo_2BDM.webp') ?>"
+                        alt="2BDM ARCHITECTURE"
+                        width="<?= $imageData[0] ?>"
+                        height="<?= $imageData[1] ?>"
+                    >
+                </a>
             </div>
             <nav class="navigation">
                 <div class="menu">

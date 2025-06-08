40b07bc0977511f7200ea3ba2e5b0bb90abb3d45
Rapkalin <raphael@myminicom.com>
2025-06-08 18:08:29 +0200

    feat(2BDM-5): add block images component

---
+++ website/app/themes/2bdm/components/block_images.php
@@ -0,0 +1,21 @@
+<?php
+$images = get_sub_field('images');
+$align = get_sub_field('align');
+$size = 'full'; // (thumbnail, medium, large, full or custom size)
+if( $images ): ?>
+    <section class="gallery-images">
+        <?php foreach( $images as $i => $image ):
+            $srcset = wp_get_attachment_image_srcset( $image['ID']);
+            $align = $i === 0 ? $align : ( $align === 'right' ? 'left' : 'right' );
+        ?>
+            <img
+                src="<?php header_image(); ?>"
+                srcset="<?php echo esc_attr( $srcset ); ?>"
+                alt="<?= $image['alt'] ?? $image['title'] ?>"
+                width="<?= $image['width'] ?>"
+                height="<?= $image['height'] ?>"
+                class="align-<?= $align ?>"
+            >
+        <?php endforeach; ?>
+    </section>
+<?php endif; ?>

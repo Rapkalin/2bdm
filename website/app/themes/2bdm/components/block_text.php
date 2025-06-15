faeceec41c706d8d30a456b2a86820f6c592328a
Rapkalin <raphael@myminicom.com>
2025-06-15 20:49:39 +0200

    feat(2BDM-5): add block text

---
+++ website/app/themes/2bdm/components/block_text.php
@@ -0,0 +1,4 @@
+<?php $align = get_sub_field('text_alignment') ?>
+<div class="text-wrapper main-wrapper text-align-<?= $align ?>">
+    <div class="text-content"><?= get_sub_field('text_content'); ?></div>
+</div>

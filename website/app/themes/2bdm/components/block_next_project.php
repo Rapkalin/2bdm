544ea597ba37cdeb3c74a5d01a6d1766c904e187
Rapkalin <raphael@myminicom.com>
2025-06-30 08:41:33 +0200

    feat(2BDM-5): init add next project

---
+++ website/app/themes/2bdm/components/block_next_project.php
@@ -0,0 +1,24 @@
+<div class="next-project-wrapper main-wrapper">
+    <?php
+        $next_project = get_sub_field('next_project')[0];
+        $next_project_banner = get_field('header_banner', $next_project->ID);
+        $srcset = wp_get_attachment_image_srcset( $next_project_banner['image']['ID']);
+    ?>
+    <div class="next-project-links">
+        <div class="previous-project">Projet précédent</div>
+        <a class="all-projects" href="/projects">Tous les projets</a>
+    </div>
+    <div class="next-project-container">
+        <h2><?= $next_project->title ?></h2>
+        <ul>
+            <li><?= $next_project->post_excerpt ?></li>
+        </ul>
+        <img
+            src="<?= $next_project_banner['image']['url']; ?>"
+            srcset="<?php echo esc_attr( $srcset ); ?>"
+            alt="<?= $next_project_banner['image']['title']; ?>"
+            height="<?= $next_project_banner['image']['height']; ?>"
+            width="<?= $next_project_banner['image']['width']; ?>"
+        >
+    </div>
+</div>

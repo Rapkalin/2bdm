d6fc14d3e11719a317b73d2f9cf403ab956adb05
Rapkalin <raphael@myminicom.com>
2025-07-14 16:32:42 +0200

    feat(2BDM-4): add accordions behavior and style

--- website/app/themes/2bdm/inc/utils.php
+++ website/app/themes/2bdm/inc/utils.php
@@ -7,3 +7,21 @@ function asset($path): string
 {
     return get_template_directory_uri() . '/assets/images/' . $path;
 }
+
+function get_terms_hierarchy(string $taxonomy): array
+{
+    $parent_terms = get_terms([
+        'taxonomy' => $taxonomy,
+        'parent' => 0,
+    ]);
+
+    $terms = [];
+    foreach ($parent_terms as $parent_term) {
+        $terms[$parent_term->name] = get_terms([
+            'taxonomy' => $taxonomy,
+            'child_of' => $parent_term->term_id,
+        ]);
+    }
+
+    return $terms;
+}
--- website/app/themes/2bdm/page-projects.php
+++ website/app/themes/2bdm/page-projects.php
@@ -11,6 +11,28 @@ if (have_rows('header_slider')) :
     get_template_part("components/block_header_slider");
 endif;
 
+$terms = get_terms_hierarchy('2bdm-projects');
+foreach ($terms as $parent_name => $child_terms) {
+    // Utilisez le slug ou un identifiant unique pour l'attribut id
+    $parent_slug = sanitize_title($parent_name);
+    ?>
+    <div class="parent-term">
+        <button class="accordion" data-target="#<?= $parent_slug ?>">
+            <?= $parent_name ?>
+        </button>
+        <div id="<?= $parent_slug ?>" class="accordion-content">
+            <?php
+            foreach ($child_terms as $child_term) : ?>
+                <div class="child-term" data-term-id="<?= $child_term->term_id ?>">
+                    <?= $child_term->name ?>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+    <?php
+}
+
+
 // Initial query to load only 4 projects
 $query = new WP_Query(array(
     'post_type' => 'projects',

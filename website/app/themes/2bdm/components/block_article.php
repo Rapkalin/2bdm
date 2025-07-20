f3a1c41e8fed7c754fbbad4be301a9a14bf40790
Rapkalin <raphael@myminicom.com>
2025-07-20 21:47:40 +0200

    feat(2BDM-7): add next article structure

---
+++ website/app/themes/2bdm/components/block_article.php
@@ -0,0 +1,31 @@
+<?php
+    $article = $args['article'];
+    $next_article_banner = get_field('thumbnail_image', $article->ID);
+    $srcset = wp_get_attachment_image_srcset( $next_article_banner['ID']);
+
+    /*
+     * We only display the first taxonomy
+     * So even though there are several contributed we only take the first one
+     */
+    $taxonomy = get_the_terms($article->ID, '2bdm-articles')[0];
+?>
+
+<div class="next-article-container">
+    <a href="<?= get_permalink() ?>">
+        <h2><?= get_field('title', $article->ID) ?></h2>
+        <div class="next-article-img">
+            <img
+                src="<?= $next_article_banner['url']; ?>"
+                srcset="<?php echo esc_attr( $args['srcset'] ); ?>"
+                alt="<?= $next_article_banner['title']; ?>"
+                height="<?= $next_article_banner['height']; ?>"
+                width="<?= $next_article_banner['width']; ?>"
+            >
+            <div class="next-article-button">
+                <button class="classic-button">
+                    <?= $taxonomy->name ?>
+                </button>
+            </div>
+        </div>
+    </a>
+</div>

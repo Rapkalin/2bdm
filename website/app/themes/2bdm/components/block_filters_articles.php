d838af569a9bb2b41f469e34d7f3d576d8d42ec2
Rapkalin <raphael@myminicom.com>
2025-07-28 01:08:09 +0200

    feat(2BDM-8) - Add articles list page

---
+++ website/app/themes/2bdm/components/block_filters_articles.php
@@ -0,0 +1,19 @@
+<div class="filters-container filters-container-articles main-wrapper" style="
+    margin-top: 50px;
+">
+    <div class="fc-title">Actualités</div>
+    <div class="fc-terms-container">
+        <div id="all" class="filter-content">
+            <div class="filter-term selected" data-term-id="all">
+                Tout
+            </div>
+        </div>
+        <?php foreach ($args['terms'] as $term) : ?>
+            <div id="<?= $term->slug ?>" class="filter-content">
+                <div class="filter-term" data-term-id="<?= $term->term_id ?>">
+                    <?= $term->name ?>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
---
+++ website/app/themes/2bdm/page-articles.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Template Name: Grille des articles
+ *
+ * @package WordPress
+ */
+
+get_header();
+
+?>
+    <div class="article-container articles-grid-header-container">
+        <div class="articles-grid-title"><?= get_field('title') ?></div>
+        <div class="articles-grid-description"><?= get_field('description') ?></div>
+    </div>
+<?php
+
+$terms =     $parent_terms = get_terms([
+    'taxonomy' => '2bdm-articles',
+    'parent' => 0,
+]);
+get_template_part("components/block_filters_articles", args: ['terms' => $terms]);
+
+// Initial query to load only 4 articles
+$tax_query = [];
+if (isset($_GET['terms']) && $_GET['terms'] !== 'all') {
+    $terms = explode(',', $_GET['terms']);
+    $tax_query = [
+        [
+            'taxonomy' => '2bdm-articles',
+            'field' => 'term_id',
+            'terms' => $terms,
+        ]
+    ];
+}
+
+$query = new WP_Query(
+    [
+        'post_type' => 'articles',
+        'post_status' => 'publish',
+        'posts_per_page' => 4,
+        'paged' => 1,
+        'tax_query' => $tax_query
+    ]
+);
+
+// Get the total number of projects
+$total_articles = $query->found_posts;
+?>
+    <div class="main-wrapper next-articles-wrapper">
+            <div class="next-articles-container">
+                <?php while ($query->have_posts()): $query->the_post(); ?>
+                    <?php get_template_part("components/block_article", args: [
+                        'article' => get_post(),
+                    ]) ?>
+                <?php endwhile; ?>
+            </div>
+    </div>
+
+    <!-- We display the Button load more projects only if there are more than 4 projects left -->
+<?php if ($total_articles > 4) : ?>
+    <div class="button-load-more">
+        <button
+            id="load-more"
+            class="classic-button"
+            data-projects-left="<?= $total_articles ?>"
+            data-page="1"
+            data-url="<?php echo admin_url('admin-ajax.php'); ?>"
+        >
+            Voir plus de projets
+            <span class="svg-plus"><?php get_template_part("components/svg-plus"); ?></span>
+        </button>
+    </div>
+<?php endif;
+
+wp_reset_query();
+get_footer();
+?>

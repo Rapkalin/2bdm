1ddb2885cd91eb6224048dc9d120753f95a65269
Rapkalin <raphael@myminicom.com>
2025-08-12 19:37:39 +0200

    feat(2BDM-6): Init page agency structure

---
+++ website/app/themes/2bdm/page-agency.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Template Name: Page agence
+ *
+ * @package WordPress
+ */
+
+get_header(args:['color-logo' => '__grey']);
+
+?>
+<div class="agency-header-container">
+    <h1 class="agency-title"><?= get_field('title') ?></h1>
+    <div class="agency-description"><?= get_field('description') ?></div>
+</div>
+
+<?php if (have_rows('content_blocks')) :
+while (have_rows('content_blocks')) : the_row(); ?>
+<section class="content-blocks">
+    <?php switch(get_row_layout()) {
+        case 'image_full':
+            get_template_part("components/block_image_full");
+            break;
+        case 'details':
+            // get_template_part("components/block_details");
+            break;
+        case 'details_full':
+            // get_template_part("components/block_details_full");
+            break;
+        case 'numbers':
+            // get_template_part("components/block_numbers");
+            break;
+        case 'quote':
+            // get_template_part("components/block_quote");
+            break;
+        case 'team':
+            // get_template_part("components/block_team");
+            break;
+        case 'awards':
+            // get_template_part("components/block_awards");
+            break;
+        case 'publications':
+            // get_template_part("components/block_publications");
+            break;
+    } ?>
+</section>
+<?php endwhile;
+endif;
+
+get_footer();
+?>

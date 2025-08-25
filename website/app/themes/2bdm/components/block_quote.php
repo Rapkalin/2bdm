122308fd786f86b8d2b8bd8d6fc6b737a481623b
Rapkalin <raphael@myminicom.com>
2025-08-25 18:50:58 +0200

    feat(2BDM-6): Init block quote structure

--- website/app/themes/2bdm/components/block_quote.php
+++ website/app/themes/2bdm/components/block_quote.php
@@ -1,8 +1,9 @@
-<?php
-dd('quote');
-?>
-<section class="section-block-quote"
-         style='background-image: url("<?= get_sub_field('image')['url']; ?>")'
->
-    <p class="block-quote"><?= get_sub_field('quote') ?></p>
-</section>
+<div class="section-block-quote main-wrapper">
+    <div class="banner" style="background-image: url(<?= get_sub_field('image')['url']; ?>);">
+        <h2 class="block-quote">
+            <?php get_template_part('components/svg-quote') ?>
+            Chez 2BDM, nous pensons que l’excellence naît de la transmission, de la collaboration et du respect de l’authenticité.
+            Chaque projet est une promesse : celle de faire perdurer un savoir-faire d’excellence, résolument tourné vers l’innovation et l’intervention responsable.
+        </h2>
+    </div>
+</div>

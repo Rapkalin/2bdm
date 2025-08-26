3773a6b910229478e829f609ffc254417dbdea21
Rapkalin <raphael@myminicom.com>
2025-08-26 14:40:44 +0200

    feat(2BDM-6): Add card info rewards structure

--- website/app/themes/2bdm/components/block_rewards.php
+++ website/app/themes/2bdm/components/block_rewards.php
@@ -23,12 +23,47 @@
             <div class="card-infos">
                 <div class="card-infos-header">
                     <div class="card-infos-header-title">Hotel de la Marine</div>
-                    <div class="card-infos-header-location">Paris VIII</div>
+                    <div class="card-infos-header-location">
+                        <?php get_template_part('components/svg-bullet') ?>
+                        Paris VIII
+                    </div>
+                </div>
+                <div class="card-button">
+                    Voir le projet
+                    <?php get_template_part("components/svg-arrow-right"); ?>
+                </div>
+                <div class="card-description">La verrière de l’Hôtel de la Marine, conçue par  Christophe Bottineau et Hugh Dutton, a été récompensée pour son audace et son innovation architecturale.<br>
+                    La verrière de l’Hôtel de la Marine, conçue par  Christophe Bottineau et Hugh Dutton, a été récompensée pour son audace et son innovation architecturale.
+                    <br>La verrière de l’Hôtel de la Marine, conçue par  Christophe Bottineau et Hugh Dutton, a été récompensée pour son audace et son innovation architecturale.
                 </div>
             </div>
-            <div class="card-button">Voir le projet</div>
-            <div class="card-description">La verrière de l’Hôtel de la Marine, conçue par  Christophe Bottineau et Hugh Dutton, a été récompensée pour son audace et son innovation architecturale.<br>
-                La verrière de l’Hôtel de la Marine, conçue par  Christophe Bottineau et Hugh Dutton, a été récompensée pour son audace et son innovation architecturale.</div>
+        </div>
+        <div class="block-rewards-card">
+            <div class="card-header-container">
+                <img class="card-logo" src="<?= asset('card-logo.png') ?>" alt="card-logo">
+                <div class="card-header">
+                    <div class="card-header-title">Prix Européen d'architecture Philippe Blabla 2025</div>
+                    <div class="card-header-category">
+                        <?php get_template_part('components/svg-bullet') ?>
+                        Catégorie "Innover"
+                    </div>
+                </div>
+            </div>
+            <img class="card-image" src="<?= asset('card-image.jpg') ?>" alt="card-image">
+            <div class="card-infos">
+                <div class="card-infos-header">
+                    <div class="card-infos-header-title">Hotel de la Marine</div>
+                    <div class="card-infos-header-location">
+                        <?php get_template_part('components/svg-bullet') ?>
+                        Paris VIII
+                    </div>
+                </div>
+                <div class="card-button">
+                    Voir le projet
+                    <?php get_template_part("components/svg-arrow-right"); ?>
+                </div>
+                <div class="card-description">La verrière de l’Hôtel de la Marine, conçue par  Christophe Bottineau et Hugh Dutton, a été récompensée pour son audace et son innovation architecturale.</div>
+            </div>
         </div>
         <div class="block-rewards-card">
             <div class="card-header-container">
@@ -45,12 +80,19 @@
             <div class="card-infos">
                 <div class="card-infos-header">
                     <div class="card-infos-header-title">Hotel de la Marine</div>
-                    <div class="card-infos-header-location">Paris VIII</div>
+                    <div class="card-infos-header-location">
+                        <?php get_template_part('components/svg-bullet') ?>
+                        Paris VIII
+                    </div>
+                </div>
+                <div class="card-button">
+                    Voir le projet
+                    <?php get_template_part("components/svg-arrow-right"); ?>
+                </div>
+                <div class="card-description">La verrière de l’Hôtel de la Marine, conçue par  Christophe Bottineau et Hugh Dutton, a été récompensée pour son audace et son innovation architecturale.<br>
+                    La verrière de l’Hôtel de la Marine, conçue par  Christophe Bottineau et Hugh Dutton, a été récompensée pour son audace et son innovation architecturale.
                 </div>
             </div>
-            <div class="card-button">Voir le projet</div>
-            <div class="card-description">La verrière de l’Hôtel de la Marine, conçue par  Christophe Bottineau et Hugh Dutton, a été récompensée pour son audace et son innovation architecturale.<br>
-                La verrière de l’Hôtel de la Marine, conçue par  Christophe Bottineau et Hugh Dutton, a été récompensée pour son audace et son innovation architecturale.</div>
         </div>
         <div class="block-rewards-card">
             <div class="card-header-container">
@@ -67,35 +109,17 @@
             <div class="card-infos">
                 <div class="card-infos-header">
                     <div class="card-infos-header-title">Hotel de la Marine</div>
-                    <div class="card-infos-header-location">Paris VIII</div>
-                </div>
-            </div>
-            <div class="card-button">Voir le projet</div>
-            <div class="card-description">La verrière de l’Hôtel de la Marine, conçue par  Christophe Bottineau et Hugh Dutton, a été récompensée pour son audace et son innovation architecturale.<br>
-                La verrière de l’Hôtel de la Marine, conçue par  Christophe Bottineau et Hugh Dutton, a été récompensée pour son audace et son innovation architecturale.</div>
-        </div>
-        <div class="block-rewards-card">
-            <div class="card-header-container">
-                <img class="card-logo" src="<?= asset('card-logo.png') ?>" alt="card-logo">
-                <div class="card-header">
-                    <div class="card-header-title">Trophée Eiffel 2021</div>
-                    <div class="card-header-category">
+                    <div class="card-infos-header-location">
                         <?php get_template_part('components/svg-bullet') ?>
-                        Catégorie "Innover"
+                        Paris VIII
                     </div>
                 </div>
-            </div>
-            <img class="card-image" src="<?= asset('card-image.jpg') ?>" alt="card-image">
-            <div class="card-infos">
-                <div class="card-infos-header">
-                    <div class="card-infos-header-title">Hotel de la Marine</div>
-                    <div class="card-infos-header-location">Paris VIII</div>
+                <div class="card-button">
+                    Voir le projet
+                    <?php get_template_part("components/svg-arrow-right"); ?>
                 </div>
+                <div class="card-description">La verrière de l’Hôtel de la Marine, conçue par  Christophe Bottineau et Hugh Dutton, a été récompensée pour son audace et son innovation architecturale.</div>
             </div>
-            <div class="card-button">Voir le projet</div>
-            <div class="card-description">La verrière de l’Hôtel de la Marine, conçue par  Christophe Bottineau et Hugh Dutton, a été récompensée pour son audace et son innovation architecturale.<br>
-                La verrière de l’Hôtel de la Marine, conçue par  Christophe Bottineau et Hugh Dutton, a été récompensée pour son audace et son innovation architecturale.</div>
         </div>
-
     </div>
 </section>

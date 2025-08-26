66a1e81f0dbb8dd0aa952cecc6dd3111fd28c55e
Rapkalin <raphael@myminicom.com>
2025-08-26 16:32:03 +0200

    feat(2BDM-3): Init home block projects

--- website/app/themes/2bdm/partials/homepage/home_projects.php
+++ website/app/themes/2bdm/partials/homepage/home_projects.php
@@ -3,20 +3,19 @@
 if (have_rows('home_projects')) :
     $featured_projects = get_field('home_projects');
     if ($featured_projects) : ?>
-        <section id="home-projects" class="home-block">
+        <section id="home-projects" class="home-block projects-container main-wrapper">
             <?php foreach ($featured_projects as $project) : ?>
-                <a href="<?= get_permalink($project->ID) ?>">
-                    <div class="home-project">
-                        <h2><?= $project->post_title ?></h2>
-                        <p><?= $project->post_excerpt ?></p>
-                        <img
-                            alt="<?= get_field('image', $project->ID)['title'] ?>"
-                            src="<?= get_field('image', $project->ID)['url'] ?>"
-                            width="<?= get_field('image', $project->ID)['width'] ?>"
-                            height="<?= get_field('image', $project->ID)['height'] ?>"
-                        >
-                    </div>
-                </a>
+            <section class="section-next-next-project-wrapper">
+                <?php
+                    $project_banner = get_field('header_banner', $project->ID);
+
+                    get_template_part("components/block_project", args: [
+                        'project' => $project,
+                        'project_banner' => $project_banner,
+                        'srcset' => wp_get_attachment_image_srcset( $project_banner['image']['ID']),
+                    ]);
+                ?>
+            </section>
             <?php endforeach; ?>
         </section>
     <?php endif;

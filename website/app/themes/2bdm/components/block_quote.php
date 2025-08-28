8cfabfe0a38bcc08444cfc0b96ffaba5a376d4f3
Rapkalin <raphael@myminicom.com>
2025-08-28 15:16:11 +0200

    feat(2BDM-6): Dynamize block quote

--- website/app/themes/2bdm/components/block_quote.php
+++ website/app/themes/2bdm/components/block_quote.php
@@ -1,9 +1,8 @@
-<section class="section-block-quote main-wrapper">
+<section class="section-block-quote">
     <div class="banner" style="background-image: url(<?= get_sub_field('image')['url']; ?>);">
         <h2 class="block-quote">
             <?php get_template_part('components/svg-quote') ?>
-            Chez 2BDM, nous pensons que l’excellence naît de la transmission, de la collaboration et du respect de l’authenticité.
-            Chaque projet est une promesse : celle de faire perdurer un savoir-faire d’excellence, résolument tourné vers l’innovation et l’intervention responsable.
+            <?= nl2br(get_sub_field('quote')) ?>
         </h2>
     </div>
 </section>

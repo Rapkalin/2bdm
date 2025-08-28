9e7637a13ee765236a0abcdbff324b355019ef5f
Rapkalin <raphael@myminicom.com>
2025-08-28 15:19:27 +0200

    feat(2BDM-6): Dynamize block numbers

--- website/app/themes/2bdm/components/block_numbers.php
+++ website/app/themes/2bdm/components/block_numbers.php
@@ -1,17 +1,11 @@
 <section class="section-block-numbers">
-    <div class="block-numbers-title">En chiffres</div>
+    <div class="block-numbers-title"><?= get_sub_field('title') ?></div>
     <div class="block-numbers-container">
-        <div class="block-number">
-            <div class="number">30</div>
-            <div class="title">ans d'expérience</div>
-        </div>
-        <div class="block-number">
-            <div class="number">3</div>
-            <div>Sites</div>
-        </div>
-        <div class="block-number">
-            <div class="number">75</div>
-            <div>collaborateurs</div>
-        </div>
+        <?php foreach (get_sub_field('numbers') as $number): ?>
+            <div class="block-number">
+                <div class="number"><?= $number['nombre'] ?></div>
+                <div class="title"><?= $number['text'] ?></div>
+            </div>
+        <?php endforeach; ?>
     </div>
 </section>

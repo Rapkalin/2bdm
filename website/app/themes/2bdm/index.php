e73b93f2174b0818643b9916cad7bb87c2c4e0a9
Rapkalin <raphael@myminicom.com>
2025-09-04 10:05:49 +0200

    feat(2BDM-3): Rollback words cover style and structure

--- website/app/themes/2bdm/index.php
+++ website/app/themes/2bdm/index.php
@@ -13,7 +13,16 @@ $homeCover = get_field('home_cover');
     </div>
 <?php endif; ?>
 
-<section class="section-block-home-cover" style='background-image: url("<?= $homeCover['image']['url']; ?>")'>
+<section class="section-block-home-cover"
+         style='background-image: url("<?= $homeCover['image']['url']; ?>")'
+>
+    <div class="block-cover-words">
+        <?php foreach($homeCover['cover_words'] as $i => $word): ?>
+            <div class="word">
+                <?= $word['cover_word'] ?>
+            </div>
+        <?php endforeach; ?>
+    </div>
 </section>
 
 <span id="first-section"></span>

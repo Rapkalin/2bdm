a3485e82765dfa5375ec1e31ecdb574b0872c0c3
Rapkalin <raphael@myminicom.com>
2025-09-11 16:31:55 +0200

    feat(2BDM-14): Fix animation millions and remove some element to be animated

--- website/app/themes/2bdm/assets/scripts/behavior_numbers_animation.js
+++ website/app/themes/2bdm/assets/scripts/behavior_numbers_animation.js
@@ -4,7 +4,7 @@ const behavior_numbers_accordion = {
     init() {
         // Detect number in text
         const textElements = document.querySelectorAll('body *:not(script):not(style):not(noscript):not(.no-numbers-animation)');
-        const numberRegex = /\b\d+(\.\d+)?\b/g;
+        const numberRegex = /\b\d+(?: \d+)*\b/g;
 
         textElements.forEach(element => {
             if (element.children.length === 0 && element.textContent.trim() !== '') {
@@ -26,12 +26,19 @@ const behavior_numbers_accordion = {
             entries.forEach(entry => {
                 if (entry.isIntersecting) {
                     const element = entry.target;
-                    const countTo = parseFloat(element.getAttribute('data-count'));
+                    const countTo = parseInt(element.getAttribute('data-count').replace(/ /g, ''), 10);
                     const countUp = new CountUp(element, countTo, {
                         duration: 2,
-                        decimalPlaces: countTo % 1 !== 0 ? 2 : 0,
+                        formattingFn: (n) => {
+                            return n.toLocaleString('fr-FR');
+                        }
                     });
-                    if (!countUp.error) countUp.start();
+
+                    if (!countUp.error) {
+                        countUp.start();
+                    } else {
+                        console.error('Erreur CountUp pour l\'élément :', element);
+                    }
                     observer.unobserve(element);
                 }
             });

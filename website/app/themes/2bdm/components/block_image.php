85d13ce1e13d031288e557ab06e8a19ca5033f9b
Rapkalin <raphael@myminicom.com>
2025-09-21 18:04:11 +0200

    feat(develop): add article image size and alignment

--- website/app/themes/2bdm/components/block_image.php
+++ website/app/themes/2bdm/components/block_image.php
@@ -1,8 +1,12 @@
 <?php
 $image = get_sub_field('image');
 if( $image ): ?>
-    <?php $srcset = wp_get_attachment_image_srcset( $image['ID']); ?>
-    <section class="section-block-article-image">
+    <?php
+        $srcset = wp_get_attachment_image_srcset( $image['ID']);
+        $align = get_sub_field('image_alignment');
+        $size = get_sub_field('image_size') ? 'big' : 'small';
+    ?>
+    <section class="section-block-article-image img-align-<?= $align ?> img-size-<?= $size ?>">
         <img
             src="<?php header_image(); ?>"
             srcset="<?php echo esc_attr( $srcset ); ?>"

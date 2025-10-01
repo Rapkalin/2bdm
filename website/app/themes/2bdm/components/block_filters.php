e04a7c0e9489ec0cfd610d1cf3c7f7fbc0d130cb
Rapkalin <rkalinowski@steamulo.com>
2025-10-01 13:54:43 +0200

    feat(develop): fix display screen

--- website/app/themes/2bdm/components/block_filters.php
+++ website/app/themes/2bdm/components/block_filters.php
@@ -1,4 +1,4 @@
-<div class="filters-container main-wrapper max-width-container" id="filters-container">
+<div class="filters-container main-wrapper" id="filters-container">
     <h3 class="fc-title">
         <?php get_template_part('components/svg-bullet') ?>
         Projects

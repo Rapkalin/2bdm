b9b577535879e68d09bd7c1bb36c9585ea2e15ab
Rapkalin <rkalinowski@steamulo.com>
2025-10-01 14:00:02 +0200

    feat(develop): fix display screen wide

--- website/app/themes/2bdm/single-projects.php
+++ website/app/themes/2bdm/single-projects.php
@@ -15,31 +15,34 @@ if (have_rows('header_banner')) : the_row() ?>
     ?>
 <?php endif;
 
-if (have_rows('content_blocks')) :
-    while (have_rows('content_blocks')) : the_row(); ?>
-        <section class="content-blocks">
-            <?php switch(get_row_layout()) {
-                case 'text':
-                    get_template_part("components/block_text");
-                    break;
-                case 'images':
-                    get_template_part("components/block_images");
-                    break;
-                case 'data_table':
-                    get_template_part("components/block_table");
-                    break;
-                case 'carousel':
-                    get_template_part("components/block_carousel");
-                    break;
-                case 'introduction':
-                    get_template_part("components/block_introduction", args: [
-                        'extraClasses' => ['main-wrapper', 'project-intro-wrapper']
-                    ]);
-                    break;
-            } ?>
-        </section>
-    <?php endwhile;
-endif;
+if (have_rows('content_blocks')) : ?>
+    <div class="max-width-container">
+        <?php while (have_rows('content_blocks')) : the_row(); ?>
+            <section class="content-blocks">
+                <?php switch(get_row_layout()) {
+                    case 'text':
+                        get_template_part("components/block_text");
+                        break;
+                    case 'images':
+                        get_template_part("components/block_images");
+                        break;
+                    case 'data_table':
+                        get_template_part("components/block_table");
+                        break;
+                    case 'carousel':
+                        get_template_part("components/block_carousel");
+                        break;
+                    case 'introduction':
+                        get_template_part("components/block_introduction", args: [
+                            'extraClasses' => ['main-wrapper', 'project-intro-wrapper']
+                        ]);
+                        break;
+                } ?>
+            </section>
+        <?php endwhile; ?>
+    </div>
+
+<?php endif;
 
 // BLOCK NEXT PROJECT
 $next_project = get_field('next_project')[0];
